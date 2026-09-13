bed71d686fa26242c0384802db775649d267529d
nabrus <34988577+nabrus@users.noreply.github.com>
2026-09-13 17:33:52 -0400

    Refactor zsh prompt for readability

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,19 +1,32 @@
 ## PROMPT ## 
-# orange 179
-# purple 98, 61
-# blue 33, 27
-# green 40
-PROMPT='%B%K{164}%F{231} %# %f%k%b%B%K{98}%F{231} %~ %f%k%b
-%B%F{33}->%f%b '
+
+# Prompt symbol
+PROMPT='%B%K{164}%F{231} %# %f%k%b'
+
+# Current directory
+PROMPT+='%B%K{98}%F{231} %~ %f%k%b'
+
+# Start command input on a new line
+PROMPT+=$'\n'
+
+# Command input indicator
+PROMPT+='%B%F{33}->%f%b '
+
 
 ## RIGHT SIDE PROMPT ##
 autoload -Uz vcs_info
-precmd_vcs_info() { vcs_info }
-precmd_functions+=( precmd_vcs_info )
+
+precmd_vcs_info() {
+  vcs_info
+}
+
+precmd_functions+=(precmd_vcs_info)
+
 setopt prompt_subst
-RPROMPT=\$vcs_info_msg_0_
+RPROMPT='$vcs_info_msg_0_'
+
 zstyle ':vcs_info:git:*' formats '%F{240}(%b)%f'
-zstyle ':vcs_info:*' enable git 
+zstyle ':vcs_info:*' enable git
 
 ## CORRECTION PROMPT ##
 SPROMPT='⚠️  Correct %B%F{196}%R%f%b to %B%F{green}%r%f%b ?
